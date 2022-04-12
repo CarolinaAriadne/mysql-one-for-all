dd453c298f9ccbc9217be1c1c0aa8cda843e6e4c
Carolina Ariadne <c.ariadneadv@gmail.com>
2022-04-12 10:58:05 -0300

    Requisito-1

--- desafio1.sql
+++ desafio1.sql
@@ -2,14 +2,6 @@ DROP DATABASE IF EXISTS SpotifyClone;
 
 CREATE DATABASE IF NOT EXISTS SpotifyClone;
 
-CREATE TABLE SpotifyClone.dados_usuario(
-    usuario_id INT PRIMARY KEY AUTO_INCREMENT,
-    nome_usuario VARCHAR (50) NOT NULL,
-    idade_usuario INT NOT NULL,
-    id_plano INT NOT NULL,
-    data_assinatura_plano DATETIME NOT NULL,
-    FOREIGN KEY(id_plano) REFERENCES planos(id_planos)
-);
 
 CREATE TABLE SpotifyClone.planos(
     id_plano INT PRIMARY KEY AUTO_INCREMENT,
@@ -17,9 +9,19 @@ CREATE TABLE SpotifyClone.planos(
     valor_plano DECIMAL (5,2) NOT NULL
 );
 
+CREATE TABLE SpotifyClone.dados_usuario(
+    usuario_id INT PRIMARY KEY AUTO_INCREMENT,
+    nome_usuario VARCHAR (50) NOT NULL,
+    idade_usuario INT NOT NULL,
+    id_plano INT NOT NULL,
+    data_assinatura_plano DATETIME NOT NULL,
+    FOREIGN KEY(id_plano) REFERENCES planos(id_plano)
+);
+
+
 CREATE TABLE SpotifyClone.artista(
-    artista_id INT PRIMARY KEY AUTO_INCREMENT,
-    nome_artista VARCHAR (50) NOT NULL
+artista_id INT PRIMARY KEY AUTO_INCREMENT,
+nome_artista VARCHAR (50) NOT NULL
 );
 
 CREATE TABLE SpotifyClone.albuns(
@@ -39,37 +41,40 @@ CREATE TABLE SpotifyClone.musicas(
 ); 
 
 CREATE TABLE SpotifyClone.historico_reproducoes(
-    usuario_id INT NOT NULL,
-    id_musica INT NOT NULL,
-    data_reproducao DATETIME NOT NULL,
-    FOREIGN KEY (usuario_id) REFERENCES dados_usuario (usuario_id),
-    FOREIGN KEY (id_musica) REFERENCES musicas (id_musica)  
+	usuario_id INT NOT NULL,
+	id_musica INT NOT NULL,
+	data_reproducao DATETIME NOT NULL,
+	FOREIGN KEY (usuario_id) REFERENCES dados_usuario (usuario_id),
+	FOREIGN KEY (id_musica) REFERENCES musicas (id_musica),
+    CONSTRAINT PRIMARY KEY(usuario_id, id_musica) 
 );
 
-CREATE TABLE SpotifyClone.usuario_segue_artistas(
-    usuario_id INT NOT NULL,
-    artista_id INT NOT NULL,
-    FOREIGN KEY (usuario_id) REFERENCES dados_usuario (usuario_id),
-    FOREIGN KEY (artista_id) REFERENCES artista (artista_id)
+CREATE TABLE SpotifyClone.seguidores(
+usuario_id INT NOT NULL,
+artista_id INT NOT NULL,
+FOREIGN KEY (usuario_id) REFERENCES dados_usuario (usuario_id),
+FOREIGN KEY (artista_id) REFERENCES artista (artista_id),
+CONSTRAINT PRIMARY KEY(usuario_id, artista_id)
 );
 
-INSERT INTO SpotifyClone.dados_usuario (nome_usuario,idade_usuario, id_plano, data_assinatura_plano) VALUES
-  ('Thati', '23', '1', '2019-10-20'),
-  ('Cintia', '35', '4', '2017-12-30'),
-  ('Bill', '20', '2', '2019-06-05'),
-  ('Roger', '45', '3', '2020-05-13'),
-  ('Norman', '58', '3', '2017-02-17'),
-  ('Patrick', '33', '4', '2017-01-06'),
-  ('Vivian', '26', '2', '2018-01-05'),
-  ('Carol', '19', '2', '2018-02-14'),
-  ('Angelina', '42', '4', '2018-04-29'),
-  ('Paul', '46', '4', '2017-01-17');
+ INSERT INTO SpotifyClone.planos(nome_plano, valor_plano) VALUES
+('gratuito', '0'),
+('universitario', '5.99'),
+('pessoal', '6.99'),
+('familiar', '7.99');
 
-INSERT INTO SpotifyClone.planos (nome_plano, valor_plano) VALUES
- ('gratuito', '0'),
- ('universitario', '5.99'),
- ('pessoal', '6.99'),
- ('familiar', '7.99');
+INSERT INTO SpotifyClone.dados_usuario(nome_usuario,idade_usuario, id_plano, data_assinatura_plano) VALUES
+('Thati', 23, 1, '2019-10-20'),
+('Cintia', 35, 4, '2017-12-30'),
+('Bill', 20, 2, '2019-06-05'),
+('Roger', 45, 3, '2020-05-13'),
+('Norman', 58, 3, '2017-02-17'),
+('Patrick', 33, 4, '2017-01-06'),
+('Vivian', 26, 2, '2018-01-05'),
+('Carol', 19, 2, '2018-02-14'),
+('Angelina', 42, 4, '2018-04-29'),
+('Paul', 46, 4, '2017-01-17');
+
 
 INSERT INTO SpotifyClone.artista(nome_artista) VALUES
 ('Walter Phoenix'),
@@ -79,7 +84,7 @@ INSERT INTO SpotifyClone.artista(nome_artista) VALUES
 ('Tyle Isle'),
 ('Fog');
 
-INSERT INTO SpotifyClone.album (nome_album, ano_lancamento, artista_id) VALUES
+INSERT INTO SpotifyClone.albuns (nome_album, ano_lancamento, artista_id) VALUES
 ('Envious', 1990, 1),
 ('Exuberant', 1993, 1),
 ('Hallowed Steam', 1995, 2),
@@ -91,7 +96,7 @@ INSERT INTO SpotifyClone.album (nome_album, ano_lancamento, artista_id) VALUES
 ('No guarantees', 2015, 5),
 ('Apparatus', 2015, 6);
 
-INSERT INTO Spotify.musicas (nome_musica, duracao_segundos, album_id) VALUES
+INSERT INTO SpotifyClone.musicas(nome_musica, duracao_segundos, album_id) VALUES
 ('Soul For US', 200, 1),
 ('Reflections Of Magic', 163, 1),
 ('Dance With Her Own', 116, 1),
@@ -106,7 +111,7 @@ INSERT INTO Spotify.musicas (nome_musica, duracao_segundos, album_id) VALUES
 ('Rock His Everything', 223, 4),
 ('Home Forever', 231, 4),
 ('Diamond Power', 241, 4),
-('Lets Be Silly', 132, 4),
+("Let's Be Silly", 132, 4),
 ('Thang Of Thunder', 240, 5),
 ('Words Of Her Life', 185, 5),
 ('Without My Streets', 176, 5),
@@ -118,64 +123,64 @@ INSERT INTO Spotify.musicas (nome_musica, duracao_segundos, album_id) VALUES
 ('Finding My Traditions', 179, 7),
 ('Walking And Man', 229, 7),
 ('Hard And Time', 135, 7),
-('Honey, Im A Lone Wolf', 150, 7),
-('She Thinks I Wont Stay Tonight', 166, 8),
-('He Heard You re Bad For Me', 154, 8),
-('He Hopes We Cant Stay', 210, 8),
+("Honey, I'm A Lone Wolf", 150, 7),
+("She Thinks I Won't Stay Tonight", 166, 8),
+("He Heard You're Bad For Me", 154, 8),
+("He Hopes We Can't Stay", 210, 8),
 ('I Know Know', 117, 8),
-('Hes Walking Away', 159, 9),
-('Hes Trouble', 138, 9),
+("He's Walking Away", 159, 9),
+("He's Trouble", 138, 9),
 ('I Heard I Want To Bo Alone', 120,9),
 ('I Ride Alone', 151,9),
 ('Honey',79,10),
 ('You Cheated On Me', 95, 10),
-('Wouldnt It Be Nice', 213, 10),
+("Wouldn't It Be Nice", 213, 10),
 ('Baby', 136, 10),
 ('You Make Me Feel So..', 83, 10);
 
 
-INSERT INTO Spotify.historico_reproducoes (musica_id, usuario_id, data_reproducao) VALUES
-  ('36', '1', '2020-02-28 10:45:55'),
-  ('25', '1', '2020-05-02 05:30:35'),
-  ('23', '1', '2020-03-06 11:22:33'),
-  ('14', '1', '2020-08-05 08:05:17'),
-  ('15', '1', '2020-09-14 16:32:22'),
-  ('34', '2', '2020-01-02 07:40:33'),
-  ('24', '2', '2020-05-16 06:16:22'),
-  ('21', '2', '2020-10-09 12:27:48'),
-  ('39', '2', '2020-09-21 13:14:46'),
-  ('6', '3', '2020-11-13 16:55:13'),
-  ('3', '3', '2020-12-05 18:38:30'),
-  ('26', '3', '2020-07-30 10:00:00'),
-  ('2', '4', '2021-08-15 17:10:10'),
-  ('35', '4', '2021-07-10 15:20:30'),
-  ('27', '4', '2021-01-09 01:44:33'),
-  ('7', '5', '2020-07-03 19:33:28'),
-  ('12', '5', '2017-02-24 21:14:22'),
-  ('14', '5', '2020-08-06 15:23:43'),
-  ('1', '5', '2020-11-10 13:52:27'),
-  ('38', '6', '2019-02-07 20:33:48'),
-  ('29', '6', '2017-01-24 00:31:17'),
-  ('30', '6', '2017-10-12 12:35:20'),
-  ('22', '6', '2018-05-29 14:56:41'),
-  ('5', '7', '2018-05-09 22:30:49'),
-  ('4', '7', '2020-07-27 12:52:58'),
-  ('11', '7', '2018-01-16 18:40:43'),
-  ('39', '8', '2018-03-21 16:56:40'),
-  ('40', '8', '2020-10-18 13:38:05'),
-  ('32', '8', '2019-05-25 08:14:03'),
-  ('33', '8', '2021-08-15 21:37:09'),
-  ('16', '9', '2021-05-24 17:23:45'),
-  ('17', '9', '2018-12-07 22:48:52'),
-  ('8', '9', '2021-03-14 06:14:26'),
-  ('9', '9', '2020-04-01 03:36:00'),
-  ('20', '10', '2017-02-06 08:21:34'),
-  ('21', '10', '2017-12-04 05:33:43'),
-  ('12', '10', '2017-07-27 05:24:49'),
-  ('13', '10', '2017-12-25 01:03:57'); 
+INSERT INTO SpotifyClone.historico_reproducoes(id_musica, usuario_id, data_reproducao) VALUES
+(36, 1, '2020-02-28 10:45:55'),
+(25, 1, '2020-05-02 05:30:35'),
+(23, 1, '2020-03-06 11:22:33'),
+(14, 1, '2020-08-05 08:05:17'),
+(15, 1, '2020-09-14 16:32:22'),
+(34, 2, '2020-01-02 07:40:33'),
+(24, 2, '2020-05-16 06:16:22'),
+(21, 2, '2020-10-09 12:27:48'),
+(39, 2, '2020-09-21 13:14:46'),
+(6, 3, '2020-11-13 16:55:13'),
+(3, 3, '2020-12-05 18:38:30'),
+(26, 3, '2020-07-30 10:00:00'),
+(2, 4, '2021-08-15 17:10:10'),
+(35, 4, '2021-07-10 15:20:30'),
+(27, 4, '2021-01-09 01:44:33'),
+(7, 5, '2020-07-03 19:33:28'),
+(12, 5, '2017-02-24 21:14:22'),
+(14, 5, '2020-08-06 15:23:43'),
+(1, 5, '2020-11-10 13:52:27'),
+(38, 6, '2019-02-07 20:33:48'),
+(29, 6, '2017-01-24 00:31:17'),
+(30, 6, '2017-10-12 12:35:20'),
+(22, 6, '2018-05-29 14:56:41'),
+(5, 7, '2018-05-09 22:30:49'),
+(4, 7, '2020-07-27 12:52:58'),
+(11, 7, '2018-01-16 18:40:43'),
+(39, 8, '2018-03-21 16:56:40'),
+(40, 8, '2020-10-18 13:38:05'),
+(32, 8, '2019-05-25 08:14:03'),
+(33, 8, '2021-08-15 21:37:09'),
+(16, 9, '2021-05-24 17:23:45'),
+(17, 9, '2018-12-07 22:48:52'),
+(8, 9, '2021-03-14 06:14:26'),
+(9, 9, '2020-04-01 03:36:00'),
+(20, 10, '2017-02-06 08:21:34'),
+(21, 10, '2017-12-04 05:33:43'),
+(12, 10, '2017-07-27 05:24:49'),
+(13, 10, '2017-12-25 01:03:57'); 
 
 
-INSERT INTO Spotify.usuario_segue_artistas (usuario_id, artista_id) VALUES
+INSERT INTO SpotifyClone.seguidores(usuario_id, artista_id) VALUES
 (1, 1),
 (1, 4),
 (1, 3),
@@ -198,6 +203,9 @@ INSERT INTO Spotify.usuario_segue_artistas (usuario_id, artista_id) VALUES
 (9, 3),
 (10, 2),
 (10, 6);
+
+
+
 
 
 
