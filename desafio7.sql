52a85a351eeefc83a27e761a9baa087b152f978f
Carolina Ariadne <c.ariadneadv@gmail.com>
2022-04-13 11:53:57 -0300

    Requisito-7

---
+++ desafio7.sql
@@ -0,0 +1,12 @@
+SELECT
+	art.nome_artista AS artista,
+    nome_album AS album,
+    COUNT(usuario_id) AS seguidores
+FROM
+    SpotifyClone.artista AS art
+        INNER JOIN
+    SpotifyClone.albuns AS alb ON art.artista_id = alb.artista_id
+        INNER JOIN
+    SpotifyClone.seguidores AS seg ON art.artista_id = seg.artista_id
+    GROUP BY art.nome_artista, alb.nome_album
+	ORDER BY seguidores DESC, artista, album;
